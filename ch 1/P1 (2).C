3c37cb96cb4c9a57c22a0259258c355039e346d5
ansh <anshisamaliya56@gmail.com>
2022-09-22 09:48:13 +0530

    new upload

---
+++ ch 1/P1 (2).C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("WAP to print your Name   : Ansh Isamaliya");
+	printf("\nWAP to print your Age    : 18");
+	printf("\nWEP to print your School : R&W");
+
+	getch();
+}
